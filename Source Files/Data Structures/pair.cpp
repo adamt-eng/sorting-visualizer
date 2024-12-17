e65c2aac757d58c075baed08ab6c6d9a1d356a81
Adam <23P0001@eng.asu.edu.eg>
2024-12-18 00:25:57 +0200

    Added more features to the Pair class.
    1. Copy constructor
    2. make_pair
    3. Assignment operator
    4. Comparison operators

--- Source Files/Data Structures/pair.cpp
+++ Source Files/Data Structures/pair.cpp
@@ -4,6 +4,27 @@ template <typename T, typename U>
 Pair<T,U>::Pair(const T& f, const U& s) : first(f), second(s) {}
 
 template <typename T, typename U>
+Pair<T, U>::Pair(const Pair& other) : first(other.first), second(other.second) {}
+
+template <typename T, typename U>
+Pair<T, U>& Pair<T, U>::operator=(const Pair& other)
+{
+    if (this != &other)
+    {
+        first = other.first;
+        second = other.second;
+    }
+
+    return *this;
+}
+
+template <typename T, typename U>
+Pair<T, U> make_pair(const T& first, const U& second)
+{
+    return Pair<T, U>(first, second);
+}
+
+template <typename T, typename U>
 bool Pair<T,U>::operator<(const Pair& other) const
 {
     return (first < other.first) || ((first == other.first) && (second < other.second));
@@ -13,4 +34,28 @@ template <typename T, typename U>
 bool Pair<T,U>::operator>(const Pair& other) const
 {
     return other < *this;
+}
+
+template <typename T, typename U>
+bool Pair<T, U>::operator<=(const Pair& other) const
+{
+    return !(*this > other);
+}
+
+template <typename T, typename U>
+bool Pair<T, U>::operator>=(const Pair& other) const
+{
+    return !(*this < other);
+}
+
+template <typename T, typename U>
+bool Pair<T, U>::operator==(const Pair& other) const
+{
+    return first == other.first && second == other.second;
+}
+
+template <typename T, typename U>
+bool Pair<T, U>::operator!=(const Pair& other) const
+{
+    return !(*this == other);
 }
